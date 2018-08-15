3757f9b05ebcfcf4018052028bece6e895cbe7e7
liyo1242 <liyo1242@gmail.com>
2018-08-15 17:23:01 +0800

    fix day zero

--- app/scripts/calendarDynamically.js
+++ app/scripts/calendarDynamically.js
@@ -1,14 +1,13 @@
 "use strict";
 var calendar, organizer;
 
-//============================================
+// calendar days layout swipe function
 $(function(){
 	$(".days").swipe({
-	fingers:'all', swipeLeft:swipe, swipeRight:swipe, allowPageScroll:"auto"
-});
+		fingers:'all', swipeLeft:swipe, swipeRight:swipe, allowPageScroll:"auto"
+	});
 
-function swipe(event, direction, distance, duration, fingerCount, fingerData, currentDirection) {
-        //console.log([event, direction, distance, duration, fingerCount, fingerData, currentDirection]);
+	function swipe(event, direction, distance, duration, fingerCount, fingerData, currentDirection) {
 	    console.log("fuckfuckfuck4+ direction " + direction );
         if(direction == "left"){
     	  	$("#calendarContainer-month-next").click();
@@ -18,95 +17,66 @@ function swipe(event, direction, distance, duration, fingerCount, fingerData, cu
     }
 });
 
-$('.appointDayAdd').click(function() {
-    console.log($('#organizerContainer-date').html());
-});
-
+// delete button click function
 $('#gg').click(function(){
-		console.log('ff4');
-		if($('#gg').attr('button-data') != ""){
-			if(confirm("確定要刪除嗎?")){
-				console.log('ffffo4');
-				var data = {
-					id: $('#gg').attr('button-data')
-				};
-				fetch('/profile/delete', {
-			    	method: 'POST', // or 'PUT'
-				    body: JSON.stringify(data), // data can be `string` or {object}!
-				    headers: new Headers({
-						'Content-Type': 'application/json'
-					})
+	var deleteId = $('#gg').attr('button-data');
+	if(deleteId != ""){
+		if(confirm("確定要刪除嗎?")){
+			var data = {
+				id: deleteId
+			};
+			fetch('/profile/delete', {
+		    	method: 'POST',
+			    body: JSON.stringify(data),
+			    headers: new Headers({
+					'Content-Type': 'application/json'
 				})
-				$('#gg').attr('button-data',"");
-				alert("已經刪除！");
-			}
-			else{
-				alert("已經取消了刪除操作");
-			}
+			})
+			.then(data => {
+				if(data.status == 200){
+					$('#gg').attr('button-data',"");
+					alert("已經刪除！");
+				}else{
+					alert('King Arthur was assassinated')
+				}
+			})
 		}
+		else{
+			alert("已經取消了刪除操作");
+		}
+	}
 })
 
-
+// edit button click function
 $('#uu').click(function(){
-	// const eventTitle = document.getElementById('message-title');
-	// const eventtext = document.getElementById('message-text');
-	// const eventLocation = document.getElementById('message-location');
-	// const eventreMinder = document.getElementById('message-reminder');
-	// const eventreTransport = document.getElementById('message-transportation');
-
 	$('#exampleModal input').prop('disabled',false);
-
-	// var c1 = Datepick1.getDate(true).replace("/","-").replace("/","-").replace("/","-");
-	// var startT = c1 + "T" + Timepick1.getDate(true) + ":00+08:00";
-
-	// var c2 = Datepick2.getDate(true).replace("/","-").replace("/","-").replace("/","-");
-	// var endT = c2 + "T" + Timepick2.getDate(true) + ":00+08:00";
-
-	// var data = {
-	// 	start: startT,
-	// 	end: endT,
-	// 	summary: eventTitle.value, //title
-	// 	location: eventLocation.value,
-	// 	description: eventtext.value,
-	// 	id: $('#uu').attr('button-data')
-	// };
-	// 	if($('#uu').attr('button-data') != ""){
-	// 		if(confirm("確定要更新嗎?")){
-	// 			console.log('ffffo4');
-	// 			fetch('/profile/update', {
-	// 		    	method: 'POST', // or 'PUT'
-	// 			    body: JSON.stringify(data), // data can be `string` or {object}!
-	// 			    headers: new Headers({
-	// 					'Content-Type': 'application/json'
-	// 				})
-	// 			}).then((data) => {console.log(data)})
-	// 			$('#uu').attr('button-data',"");
-	// 			alert("已經更新！");
-	// 		}
-	// 		else{
-	// 			alert("已經取消更新操作");
-	// 		}
-	// 	}
 })
 
+// The moment of Modal appearance will trigger
 $('#exampleModal').on('show.bs.modal', function (event) {
-	$('#exampleModal input').prop('disabled',true);
-	console.log('fuck you show');
-  	var button = $(event.relatedTarget) // Button that triggered the modal
-  	$(".btnpos").toggleClass('btnposflip');
+
+	console.log('4+ appear');
+	var button = $(event.relatedTarget) // Button that triggered the modal
   	console.log(button.data('whatever')); // Extract info from data-* attributes
+
+	$(".btnpos").toggleClass('btnposflip');
+
   	if(button.data('whatever') != undefined){
+  		if(button.data('whatever').id != "" && button.data('whatever').id != undefined){
+			$('#exampleModal input').prop('disabled',true);
+			$('#gg').attr('button-data',button.data('whatever').id);
+	  		$('#uu').attr('button-data',button.data('whatever').id);
+	  		$('#woofbtn').attr('button-data',button.data('whatever').id);
+		}
+
 	  	var modal = $(this);
 	  	modal.find('#message-title').val(button.data('whatever').title);
 	  	modal.find('#message-text').val(button.data('whatever').text);
 	  	modal.find('#message-location').val(button.data('whatever').location);
-	  	$('#gg').attr('button-data',button.data('whatever').id);
-	  	$('#uu').attr('button-data',button.data('whatever').id);
-	  	$('#woofbtn').attr('button-data',button.data('whatever').id);
 
 	  	if(button.data('whatever').startTime != undefined && button.data('whatever').endTime != undefined ){
+	  		// the days trigger modal have already exists data
 			var time = button.data('whatever').time;
-			var d = new Date();
 			$('#startTime').val(button.data('whatever').startTime);
 			$('#startDate').val(time.slice(0,4) + '/' + time.slice(5,7) + '/' + time.slice(9));
 			// === === === ===
@@ -114,15 +84,38 @@ $('#exampleModal').on('show.bs.modal', function (event) {
 			$('#endTime').val(button.data('whatever').endTime);
 			$('#endDate').val(time.slice(0,4) + '/' + time.slice(5,7) + '/' + time.slice(9));
 		}else if(button.data('whatever').time != undefined){
+			// the days trigger modal have empty data
 			var time = button.data('whatever').time;
 			var d = new Date();
+			var dayZero = (parseInt(time.slice(9)) < 10) ? ("0" + time.slice(9)) : time.slice(9);
+			console.log(dayZero);
+			// time gap
+			var offset = 0;
+			var carry = 0;
+			if(d.getHours() < 23){
+				if(d.getMinutes() >= 45){
+					carry = 1;
+					offset = -45;
+				}else {
+					offset = 15;
+				}
+			}else if (d.getHours() == 23) {
+				if(d.getMinutes() >= 45){
+					offset = 0;
+				}else {
+					offset = 15;
+				}
+			}
+
 			$('#startTime').val(d.getHours() + ':' + d.getMinutes());
-			$('#startDate').val(time.slice(0,4) + '/' + time.slice(5,7) + '/' + time.slice(9));
+			$('#startDate').val(time.slice(0,4) + '/' + time.slice(5,7) + '/' + dayZero);
 			// === === === ===
 			// === === === ===
-			$('#endTime').val(d.getHours() + ':' + d.getMinutes());
-			$('#endDate').val(time.slice(0,4) + '/' + time.slice(5,7) + '/' + time.slice(9));
+			$('#endTime').val((d.getHours() + carry) + ':' + (d.getMinutes() + offset));
+			$('#endDate').val(time.slice(0,4) + '/' + time.slice(5,7) + '/' + dayZero);
 		}
+
+		// picker content update
 		Timepick1.update();
 		Datepick1.update();
 		Timepick2.update();
@@ -131,41 +124,31 @@ $('#exampleModal').on('show.bs.modal', function (event) {
 })
 
 // modal disppear refresh calendar page
-
 $('#exampleModal').on('hidden.bs.modal', function () {
-	console.log('fuck you hidden');
+	console.log('4+ hidden');
 	$('#exampleModal input').prop('disabled',false);
     $(".btnpos").toggleClass('btnposflip');
-    // the high frequency operation maybe Waste of web resources =============== fix point
-    dataWithAjax(function(data) {
-		// initializing a new organizer object, that will use an html container to create itself
-		organizer = new Organizer("organizerContainer", // id of html container for calendar
-			calendar, // defining the calendar that the organizer is related
-			data // small part of the data of type object
-		);
-	});
 })
 
 // ================================
 $(".btnpos").click(function(){
 	var d = new Date();
+	var timeStr ="";
+
+	if(d.getMonth() + 1 >= 10){
+		timeStr = d.getFullYear() + ' ' + (d.getMonth() + 1) + '/' + d.getDate();
+	}else{
+		 timeStr = d.getFullYear() + ' 0' + (d.getMonth() + 1) + '/' + d.getDate();
+	}
+
 	var sendData = {
         id: "",
         title: "",
         text: "",
-        location: ""
+        location: "",
+        time: timeStr
     }
     $(".btnpos").attr("data-whatever",JSON.stringify(sendData));
-	$('#startTime').val(d.getHours() + ':' + d.getMinutes());
-	$('#startDate').val(d.getFullYear()	+ '/' + (d.getMonth() + 1) + '/' + d.getDate());
-	// === === === ===
-	// === === === ===
-	$('#endTime').val(d.getHours() + ':' + d.getMinutes());
-	$('#endDate').val(d.getFullYear()	+ '/' + (d.getMonth() + 1) + '/' + d.getDate());
-	Timepick1.update();
-	Datepick1.update();
-	Timepick2.update();
-	Datepick2.update();
 })
 
 // ================================
@@ -204,9 +187,22 @@ woofbtn.addEventListener('click', (e) => {
 			    headers: new Headers({
 					'Content-Type': 'application/json'
 				})
-			}).then((data) => {console.log(data)})
+			})
+			.then((res) => {
+				if(res.ok){
+					dataWithAjax(function(data) {
+						// initializing a new organizer object, that will use an html container to create itself
+						organizer = new Organizer("organizerContainer", // id of html container for calendar
+							calendar, // defining the calendar that the organizer is related
+							data // small part of the data of type object
+						);
+					});
+					alert("已經更新！");
+					return;
+				}
+				throw new Error('failed');
+			});
 			$('#woofbtn').attr('button-data',"");
-			alert("已經更新！");
 		}
 		else{
 			alert("已經取消更新操作");
@@ -223,7 +219,6 @@ woofbtn.addEventListener('click', (e) => {
 			})
 			.then((res) => {
 				if(res.ok) {
-					console.log('click ok');
 					dataWithAjax(function(data) {
 						// initializing a new organizer object, that will use an html container to create itself
 						organizer = new Organizer("organizerContainer", // id of html container for calendar
@@ -249,8 +244,8 @@ woofbtn.addEventListener('click', (e) => {
 	// ============================================= fix point end
 	eventTitle.value = "";
 	eventLocation.value = "";
-	eventreTransport.value = "";
-	eventreMinder.value = "";
+	//eventreTransport.value = "";
+	//eventreMinder.value = "";
 	eventtext.value = "";
 })
 // initializing a new calendar object, that will use an html container to create itself
@@ -289,7 +284,6 @@ const Datepick2 = new Picker(document.querySelector('#endDate'), {
 function dataWithAjax(callback) {
 	fetch('/profile/askEvent', {method: 'GET'})
 	.then((res) => {
-		//console.log(res.json);
 		if(res.ok) return res.json();
 		throw new Error('failed');
 	})
