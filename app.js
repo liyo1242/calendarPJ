318b730e6b2018cc05f0e74175957c8896c09889
liyo1242 <liyo1242@gmail.com>
2018-08-28 15:07:44 +0800

    connect to  line

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const profileRoutes = require('./routes/profile-routes');
-const apis = require('./gandalf/api.js');
 const app = express();
 const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose');
