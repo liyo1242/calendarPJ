9672ede0dce15614e5cdda8842f1d6b918006bd4
liyo1242 <liyo1242@gmail.com>
2018-09-19 16:29:15 +0800

    ad"

--- app/gandalf/user.js
+++ app/gandalf/user.js
@@ -1,8 +1,8 @@
 const grant_type = "authorization_code";
 const redirect_uri = "http://bn-calendar.herokuapp.com/cookie";
 // const redirect_uri = "http://localhost:8080/html/orderList/cookie.html";
-const client_id = "1603688403";
-const client_secret = "715f9773c92f470c9d52b76e26adbf68";
+const client_id = "1608728878";
+const client_secret = "762f200bfdb76cffd77c4c915e72fbf7";
 
 var search = window.location.search.split("&");
 var s = search[0].split("=");
