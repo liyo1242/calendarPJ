e87c598059d8f5e611226f59e6275f689dfe3bff
liyo1242 <liyo1242@gmail.com>
2018-09-26 15:14:30 +0800

    fix packge

--- models/user-model.js
+++ models/user-model.js
@@ -1,12 +1,12 @@
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-// const userSchema = new Schema({
-// 	accessToken:String,
-// 	refreshToken:String,
-// 	username:String,
-// 	googleId:String
-// })
+const userSchema = new Schema({
+	accessToken:String,
+	refreshToken:String,
+	username:String,
+	googleId:String
+})
 
-// const User = mongoose.model('user',userSchema);
-// module.exports = User;
+const User = mongoose.model('user',userSchema);
+module.exports = User;
